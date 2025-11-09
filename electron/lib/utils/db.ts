1d625e6676696b3b13cbf022ca5648810f8663e5
Nivekithan <nivekithan363@gmail.com>
2025-11-09 17:02:06 +0530

    Add readme.md

--- electron/lib/utils/db.ts
+++ electron/lib/utils/db.ts
@@ -1,8 +1,14 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import path from "path";
+import fs from "fs";
 
-const sqlite = new Database(path.join(process.cwd(), "data", "sqlite.db"));
+const dataDir = path.join(process.cwd(), "data");
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const sqlite = new Database(path.join(dataDir, "sqlite.db"));
 
 sqlite.pragma("journal_mode = WAL");
 
